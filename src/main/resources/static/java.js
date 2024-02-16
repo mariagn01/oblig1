39d426697a8610b74dffc105a8edcd0e3ed99a8c
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2024-02-16 13:18:24 +0100

    Laget valideringsfunksjon for inputfelter

--- src/main/resources/static/java.js
+++ src/main/resources/static/java.js
@@ -1,12 +1,15 @@
 let billetter = [];
+
 function kjopbillett () {
+
+     if (validerInputs()) {
     //Henter input-verdier fra bruker
-   let fornavn = document.getElementById("fornavn").value;
-   let etternavn = document.getElementById("etternavn").value;
-   let telefonnr = document.getElementById("telefonnr").value;
-   let epost = document.getElementById("e-post").value;
-   let film = document.getElementById("film").value;
-   let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value
+    let telefonnr = document.getElementById("telefonnr").value
+    let epost = document.getElementById("e-post").value;
+    let film = document.getElementById("film").value;
+    let antall = document.getElementById("antall").value;
 
    //Legger verdier i et objekt og legger dette inn i array
    const billett = {film, antall, fornavn, etternavn,telefonnr,epost}
@@ -29,8 +32,52 @@ function kjopbillett () {
     document.getElementById("etternavn").value="";
     document.getElementById("telefonnr").value="";
     document.getElementById("e-post").value="";
+ }
 }
 function slettbilletter () {
     billetter = [];
     document.getElementById("billettvisning").innerHTML="Billettene er slettet";
+}
+
+function validerInputs() {
+    let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value;
+    let telefonnr = document.getElementById("telefonnr").value;
+    let epost = document.getElementById("e-post").value;
+
+    //Nullstiller feilmeldinger
+    document.getElementById("antall-feil").textContent = "";
+    document.getElementById("fornavn-feil").textContent = "";
+    document.getElementById("etternavn-feil").textContent = "";
+    document.getElementById("telefonnr-feil").textContent = "";
+    document.getElementById("epost-feil").textContent = "";
+
+    if (antall <= 0 || !Number.isInteger(parseInt(antall))) {
+        document.getElementById("antall-feil").innerHTML="Skriv inn antall";
+        return false;
+    }
+
+    if (fornavn === "") {
+        document.getElementById("fornavn-feil").innerHTML="Skriv inn fornavn";
+        return false;
+    }
+
+    if (etternavn === "") {
+        document.getElementById("etternavn-feil").innerHTML="Skriv inn etternavn";
+        return false;
+    }
+
+    if (telefonnr === "" || isNaN(telefonnr)) {
+        document.getElementById("telefonnr-feil").innerHTML="Skriv inn telefonnummer";
+        return false;
+    }
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (epost === "" || !emailRegex.test(epost)) {
+        document.getElementById("epost-feil").innerHTML="Vennligst skriv inn en gyldig e-post";
+        return false;
+    }
+
+    return true; // Returnerer true hvis alle valideringer er bestått
 }
